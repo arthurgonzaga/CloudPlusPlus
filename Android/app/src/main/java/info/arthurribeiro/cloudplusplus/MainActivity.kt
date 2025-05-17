56ee98997709be8d1d0b0845c70cc55d1348791b
Arthur Gonzaga <arthurgonzagaxyz@gmail.com>
2025-05-17 08:13:10 -0300

    Initial commit

---
+++ Android/app/src/main/java/info/arthurribeiro/cloudplusplus/MainActivity.kt
@@ -0,0 +1,28 @@
+package info.arthurribeiro.cloudplusplus
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import info.arthurribeiro.cloudplusplus.ui.theme.CloudPlusPlusTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CloudPlusPlusTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
